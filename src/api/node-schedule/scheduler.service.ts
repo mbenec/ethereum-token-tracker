a647a1083b3b7d006d3efa6dab7e90f1720c4644
Mauro Benec <mauro.benec@gmail.com>
2020-12-14 19:48:05 +0100

    add node-schedule

---
+++ src/api/node-schedule/scheduler.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { RecurrenceRule, scheduleJob } from 'node-schedule';
+import { TokenService } from '../token/token.service';
+
+@Injectable()
+export class ScheduleService {
+    constructor(private readonly tokenService: TokenService) {
+        this.insertNewTokens();
+    }
+
+    insertNewTokens() {
+        const rule = new RecurrenceRule();
+        rule.second = [0];
+        scheduleJob(rule, async () => {
+            // eslint-disable-next-line no-console
+            console.log("Fetching new tokens.");
+            await this.tokenService.insertNewTokens();
+        });
+    }
+
+}
