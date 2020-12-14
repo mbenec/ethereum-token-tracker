f57418ce0e39de218d0b4d622a58fc442c54d980
Mauro Benec <mauro.benec@gmail.com>
2020-12-14 17:19:57 +0100

    add db seed

---
+++ src/seed.ts
@@ -0,0 +1,35 @@
+import { AxiosService } from './axios/axios.service';
+import { Connection } from 'typeorm';
+import { NestFactory } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmSeedDevConfig } from '../config/seed-dev';
+import { Logger, Module } from '@nestjs/common';
+import { TokenRepository } from './token/token.repository';
+import { ClassValidatorService } from './validators/class-validator.service';
+import { TokenService } from './token/token.service';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useClass: TypeOrmSeedDevConfig,
+        }),
+    ],
+    providers: [AxiosService, TokenRepository, Logger, ClassValidatorService, TokenService],
+})
+class SeedModule {}
+
+async function bootstrap() {
+    const app = await NestFactory.createApplicationContext(SeedModule);
+
+    const connection: Connection = app.get(Connection);
+    await connection.synchronize(true);
+    const tokenService: TokenService = app.get(TokenService);
+
+    const count = await tokenService.insertAllTokens();
+    // eslint-disable-next-line no-console
+    console.log("Inserted total of " + count + " tokens.");
+    // eslint-disable-next-line no-console
+    console.log('Database seeded.');
+}
+
+bootstrap();
